2955706b5026fe8b704e1b0cc1156c1ca4b0c6c0
Rafael Tosta <rafa.ecomp@gmail.com>
2024-06-16 10:23:58 -0300

    feat: Sequelize configs

---
+++ src/main/sequelize/config/database.ts
@@ -0,0 +1,20 @@
+//Configurações de conexão com o banco de dados.
+
+export default {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'database_development',
+    host: '127.0.0.1',
+    dialect: 'sqlite',
+    storage: './gerped-dev.sqlite',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'sqlite',
+    storage: './gerped.sqlite',
+  },
+};
---
+++ src/main/sequelize/db.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from 'sequelize';
+import config from './config/database';
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+
+const sequelize = new Sequelize({
+    dialect: dbConfig.dialect,
+    storage: dbConfig.storage,
+    logging: console.log,
+});
+
+
+export default sequelize;
