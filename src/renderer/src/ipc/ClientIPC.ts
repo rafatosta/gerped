4a62ab1ea44fe20457d358c298d3f75f0e4a9a63
Rafael Tosta <rafa.ecomp@gmail.com>
2024-06-26 12:56:41 -0300

    Create ClientIPC.ts

---
+++ src/renderer/src/ipc/ClientIPC.ts
@@ -0,0 +1,39 @@
+import Client from "@backend/models/Client";
+import { parseError } from "@renderer/utils/parseError";
+
+class ClientIPC {
+
+  static async findAll(searchText: string, currentPage: number): Promise<{ data: Client[]; count: number }> {
+    try {
+      return await window.electron.ipcRenderer.invoke('client:findAll', searchText, currentPage);
+    } catch (err) {
+      throw parseError(err);
+    }
+  }
+
+  static async findById(id: string): Promise<Client> {
+    try {
+      return await window.electron.ipcRenderer.invoke('client:findById', id);
+    } catch (err) {
+      throw parseError(err);
+    }
+  }
+
+  static async save(client: Client): Promise<Client> {
+    try {
+      return await window.electron.ipcRenderer.invoke('client:save', client);
+    } catch (err) {
+      throw parseError(err);
+    }
+  }
+
+  static async delete(id: number): Promise<void> {
+    try {
+      await window.electron.ipcRenderer.invoke('client:delete', id);
+    } catch (err) {
+      throw parseError(err);
+    }
+  }
+}
+
+export default ClientIPC;
