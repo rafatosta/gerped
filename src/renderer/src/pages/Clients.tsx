7c824364b8a0457bccd5e9633d84a0416fb57d2f
Rafael Tosta <rafa.ecomp@gmail.com>
2024-06-26 10:55:52 -0300

    doc: Client page

--- src/renderer/src/pages/Clients.tsx
+++ src/renderer/src/pages/Clients.tsx
@@ -1,29 +1,33 @@
-import Client from '@backend/models/Client'
-import ClientFormModal from '@renderer/components/ClientFormModal'
-import Container from '@renderer/components/Container'
-import GenericTable from '@renderer/components/GenericTable'
-import PaginationControls from '@renderer/components/PaginationControls'
-import Title from '@renderer/components/Title'
-import { useClient } from '@renderer/hooks/useClient'
-import { formatPhoneNumber } from '@renderer/utils/formatPhoneNumber'
-import { FloatingLabel, TextInput } from 'flowbite-react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import Client from '@backend/models/Client' // Importa o modelo de Cliente do backend
+import ClientFormModal from '@renderer/components/ClientFormModal' // Importa o modal de formulário de Cliente
+import Container from '@renderer/components/Container' // Importa o componente de Container
+import GenericTable from '@renderer/components/GenericTable' // Importa o componente de tabela genérica
+import PaginationControls from '@renderer/components/PaginationControls' // Importa os controles de paginação
+import Title from '@renderer/components/Title' // Importa o componente de título
+import { useClient } from '@renderer/hooks/useClient' // Importa o hook personalizado useClient para operações relacionadas a Clientes
+import { formatPhoneNumber } from '@renderer/utils/formatPhoneNumber' // Importa a função utilitária para formatar números de telefone
+import { FloatingLabel } from 'flowbite-react' // Importa o componente FloatingLabel do pacote flowbite-react
+import { useState } from 'react' // Importa o hook useState do React
+import { Link } from 'react-router-dom' // Importa o componente Link do React Router
 
 function Clients() {
-  const [currentPage, setCurrentPage] = useState<number>(1)
-  const [searchText, setSearchText] = useState<string>('')
+  const [currentPage, setCurrentPage] = useState<number>(1) // Estado para controlar a página atual da lista de Clientes
+  const [searchText, setSearchText] = useState<string>('') // Estado para armazenar o texto de busca na lista de Clientes
 
-  const { data, count, save } = useClient(searchText, currentPage)
+  const { data, count, save } = useClient(searchText, currentPage) // Desestruturação do retorno do hook useClient para obter dados, contagem e função de salvamento
 
+  // Função para atualizar a página atual da lista de Clientes
   const onPageChange = (page: number) => setCurrentPage(page)
+
+  // Função para atualizar o texto de busca na lista de Clientes
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
     setSearchText(event.target.value)
 
+  // Definição das colunas da tabela de Clientes
   const columns = [
     {
       header: 'Nome',
-      accessor: (data: Client) => data.name,
+      accessor: (data: Client) => data.name, // Acessa o nome do Cliente
       className: 'whitespace-nowrap font-medium text-gray-900'
     },
     {
@@ -43,9 +47,10 @@ function Clients() {
 
   return (
     <Container>
+      {/* Título da página */}
       <Title disabled>Clientes</Title>
 
-
+      {/* Barra de busca e botão para abrir modal de formulário de Cliente */}
       <div className="grid grid-cols-3 items-start gap-x-4">
         <div className="col-span-2">
           <FloatingLabel
@@ -58,10 +63,15 @@ function Clients() {
         </div>
 
         <div className="flex justify-end">
+          {/* Modal de formulário para adicionar novo Cliente */}
           <ClientFormModal saveCliente={save} />
         </div>
       </div>
+
+      {/* Tabela de Clientes com dados e colunas definidas */}
       <GenericTable data={data} columns={columns} keyExtractor={(data: Client) => data.id} />
+
+      {/* Controles de paginação para navegar entre páginas de Clientes */}
       <PaginationControls
         currentPage={currentPage}
         totalRecords={count}
