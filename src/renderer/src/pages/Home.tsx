d07efaced19d5ce4cdf235d1715336244949b70c
Rafael Tosta <rafa.ecomp@gmail.com>
2024-07-02 19:28:18 -0300

    Update Home.tsx

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Container from "@renderer/components/Container";
-import Title from "@renderer/components/Title";
 import CalendarHeader from "@renderer/components/CalendarHeader";
 import Calendar from "@renderer/components/Calendar";
 
 import Order from '@backend/models/Order';
 import OrderIPC from '@renderer/ipc/OrderIPC';
+import Title from '@renderer/components/Title';
 
 const Home: React.FC = () => {
-  const [currentDate, setCurrentDate] = useState<Date | null>(null);
+  const [currentDate, setCurrentDate] = useState<Date>(new Date);
   const [firstOrderDate, setFirstOrderDate] = useState<Date | null>(null);
   const [orders, setOrders] = useState<Order[]>([]);
   const [count, setCount] = useState<number>(0);
@@ -24,14 +24,12 @@ const Home: React.FC = () => {
       const deliveryDates = res.data.map(order => new Date(order.deliveryDate));
       const minYear = Math.min(...deliveryDates.map(date => date.getFullYear()));
       const maxYear = Math.max(...deliveryDates.map(date => date.getFullYear()));
-      
+
       setStartYear(minYear);
       setEndYear(maxYear);
 
       console.log(minYear, maxYear);
       console.log(res.data, res.count);
-      
-      
 
       if (res.data.length > 0) {
         const firstOrder = new Date(res.data[0].deliveryDate);
@@ -69,19 +67,18 @@ const Home: React.FC = () => {
 
   return (
     <Container >
-      <Title disabled>Home Page</Title>
-      {currentDate && (
-        <>
-          <CalendarHeader 
-            currentDate={currentDate} 
-            onDateChange={setCurrentDate} 
-            startYear={startYear} 
-            endYear={endYear} 
-            goToFirstOrder={goToFirstOrder}
-          />
-          <Calendar currentDate={currentDate} firstOrderDate={firstOrderDate} orders={orders} />
-        </>
-      )}
+      <div className="flex justify-between items-center gap-4">
+        <Title disabled>Seus Pedidos</Title>
+        <p className="text-gray-500 text-lg">{count} pedidos</p>
+      </div>
+      <CalendarHeader
+        currentDate={currentDate}
+        onDateChange={setCurrentDate}
+        startYear={startYear}
+        endYear={endYear}
+        goToFirstOrder={goToFirstOrder}
+      />
+      <Calendar currentDate={currentDate} firstOrderDate={firstOrderDate} orders={orders} />
     </Container>
   );
 }
