de4084d6f6d910f3508f973343b945eb8922e069
Rafael Tosta <rafa.ecomp@gmail.com>
2024-07-08 17:41:33 -0300

    fix: react-router-dom not work in the production

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
 import About from './pages/About'
