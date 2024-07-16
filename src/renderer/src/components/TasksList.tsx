b8e63caa300fe255ad300a025f8d56742f4f5e8b
Rafael Tosta <rafa.ecomp@gmail.com>
2024-07-16 08:53:24 -0300

    Popover for Details

--- src/renderer/src/components/TasksList.tsx
+++ src/renderer/src/components/TasksList.tsx
@@ -1,7 +1,8 @@
 import Task from '@backend/models/Task'
-import { FaCheck } from 'react-icons/fa'
+import { FaCheck, FaEye } from 'react-icons/fa'
 import Title from './Title'
 import { Link } from 'react-router-dom'
+import { Button, Popover } from 'flowbite-react'
 
 interface TasksListProps {
   tasks: Task[]
@@ -13,42 +14,60 @@ function TasksList({ tasks, updateTask }: TasksListProps) {
     <div className="flex flex-col overflow-auto">
       <Title disabled>Tarefas</Title>
       <div className="overflow-auto">
-        <ul role="list" className="grid grid-cols-1 gap-4 m-2">
+        <ul role="list" className="grid grid-cols-1 gap-4 m-2 min-w-3xl">
           {tasks.map((task) => (
             <li
               key={task.id}
               className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow shadow-gray-400"
             >
-              <div className="flex w-full items-center justify-between p-4">
-                <div className="flex flex-col truncate">
-                  <h3 className="truncate font-semibold text-gray-800">{task.description}</h3>
-
-                  <Link
-                    to={`/orders/${task.Order.id}`}
-                    className="mt-1 truncate text-sm text-gray-500 hover:text-cyan-600 hover:underline"
-                  >
-                    {task.Order.theme}
-                  </Link>
-                  <Link
-                    to={`/clients/${task.Order.Client.id}`}
-                    className="mt-1 truncate text-sm text-gray-500 hover:text-cyan-600 hover:underline"
-                  >
-                    {task.Order.Client.name}
-                  </Link>
-                </div>
-              </div>
+              <h3 className=" p-4 font-semibold text-gray-800">{task.description}</h3>
               <div>
-                <div className="-mt-px flex divide-x divide-gray-200">
-                  <div className="flex w-0 flex-1">
+                <div className="grid grid-cols-2 divide-x divide-gray-200 py-2">
+
+                  <button
+                    onClick={() => updateTask(task)}
+                    className="flex justify-center items-center gap-x-1 text-sm font-semibold text-gray-900"
+                  >
+                    <FaCheck className="text-gray-400" />
+                    Concluir
+                  </button>
+
+                  <Popover
+                    aria-labelledby="default-popover"
+                    content={
+                      <div className="w-64 text-sm text-gray-500 dark:text-gray-400">
+                        <div className="border-b border-gray-200 bg-gray-100 px-3 py-2 dark:border-gray-600 dark:bg-gray-700">
+                          <h3 id="default-popover" className="font-semibold text-gray-900 dark:text-white">Detalhres da tarefa</h3>
+                        </div>
+                        <div className="flex flex-col px-3 py-2">
+                          <Link
+                            to={`/orders/${task.Order.id}`}
+                            className="mt-1 truncate text-sm text-gray-500 hover:text-cyan-600 hover:underline"
+                          >
+                            <span className='text-gray-900'>Tema: </span>{task.Order.theme}
+                          </Link>
+                          <Link
+                            to={`/clients/${task.Order.Client.id}`}
+                            className="mt-1 truncate text-sm text-gray-500 hover:text-cyan-600 hover:underline"
+                          >
+                            <span className='text-gray-900'>Cliente: </span> {task.Order.Client.name}
+                          </Link>
+                        </div>
+                      </div>
+                    }
+                  >
                     <button
-                      onClick={() => updateTask(task)}
-                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-3 text-sm font-semibold text-gray-900"
+                      className="flex justify-center items-center gap-x-1 text-sm font-semibold text-gray-900"
                     >
-                      <FaCheck className="text-gray-400" />
-                      Concluir
+                      <FaEye className='text-gray-400' />
+                      Detalhes
                     </button>
-                  </div>
+                  </Popover>
+
                 </div>
+
+
+
               </div>
             </li>
           ))}
