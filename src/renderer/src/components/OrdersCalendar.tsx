854171dc4b2b23e25fed5c5dbb8dd6df65a7697d
Rafael Tosta <rafa.ecomp@gmail.com>
2024-07-16 09:48:02 -0300

    Update OrdersCalendar.tsx

--- src/renderer/src/components/OrdersCalendar.tsx
+++ src/renderer/src/components/OrdersCalendar.tsx
@@ -15,6 +15,8 @@ import { ptBR } from 'date-fns/locale'
 import CalendarHeader from './CalendarHeader'
 import { useEffect, useRef, useState } from 'react'
 import { classNames } from '@renderer/utils/classNames'
+import { Popover } from 'flowbite-react'
+import { Link } from 'react-router-dom'
 
 interface CalendarProps {
   orders: Order[]
@@ -95,19 +97,68 @@ function Calendar({ orders }: CalendarProps) {
             )}
           </div>
 
-          <ul className=" space-y-1 overflow-auto">
+          <div className='grid grid-flow-row'>
             {getOrdersForDay(day).map((order) => (
-              <li
-                key={order.id}
-                className={classNames(
-                  'p-1 text-nowrap md:text-sm rounded-lg mt-1 overflow-hidden border truncate',
-                  eventClass
-                )}
-              >
-                {order.Client?.name}
-              </li>
+              start.getMonth() == day.getMonth() ?
+                <Popover
+                  key={order.id}
+                  aria-labelledby="default-popover"
+                  aria-disabled
+                  content={
+                    <div className="text-sm text-gray-500 dark:text-gray-400">
+                      {/* Title */}
+                      <div className="border-b border-gray-200 bg-gray-100 p-3 gap-y-3 flex flex-col justify-center items-center">
+                        <h3 id="default-popover" className="font-semibold text-xl text-gray-900">
+                          {order.theme}
+                        </h3>
+                        <h3 className="font-semibold text-gray-500">
+                          {format(order.deliveryDate, 'dd MMMM yyyy', { locale: ptBR })}
+                        </h3>
+                      </div>
+                      {/* Body */}
+                      <div className="flex flex-col p-3 gap-2">
+                        <p>Cliente: <span className='text-gray-900 font-medium'>{order.Client.name}</span> </p>
+                        <p>Serviço: <span className='text-gray-900 font-medium'>{order.Service.description}</span></p>
+                        <p>Data do pedido: <span className='text-gray-900 font-medium'>{format(order.orderDate, 'dd/MM/yyyy', { locale: ptBR })}</span></p>
+                        <p>Data da entrega: <span className='text-gray-900 font-medium'>{format(order.deliveryDate, 'dd/MM/yyyy', { locale: ptBR })}</span></p>
+                        <p>Valor: <span className='text-gray-900 font-medium'> R$ {order.price}</span></p>
+                        <p>Tarefas a concluir:
+                          <span className='text-gray-900 font-medium'>
+                            {order.countTask == 0 ? " Sem tarefas cadastradas" : ` ${order.countTaskFinished} de ${order.countTask}`}
+                          </span>
+                        </p>
+                      </div>
+                      {/* Button */}
+                      <Link
+                        to={`/orders/${order.id}`}
+                        className="flex justify-center items-center border-t p-4 w-full hover:bg-gray-100  text-sm font-semibold text-gray-900"
+                      >
+                        Abrir pedido
+                      </Link>
+
+                    </div>
+                  }
+                >
+                  <button
+                    className={classNames(
+                      'p-1 text-nowrap md:text-sm rounded-lg mt-1 overflow-hidden border truncate text-start',
+                      eventClass
+                    )}
+                  >
+                    {order.Client?.name}
+                  </button>
+                </Popover>
+                :
+                <button
+                  className={classNames(
+                    'p-1 text-nowrap md:text-sm rounded-lg mt-1 overflow-hidden border truncate text-start',
+                    eventClass
+                  )}
+                >
+                  {order.Client?.name}
+                </button>
             ))}
-          </ul>
+          </div>
         </div>
       )
       day = addDays(day, 1)
